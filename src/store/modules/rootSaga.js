f9c23eca325b5fd5c32c4481344168c46f695c29
Jayro Barbosa <jayro@MacBook-Jayro.local>
2020-01-20 10:27:59 -0300

    feature: Armazenando perfil de usuário

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,7 +1,11 @@
 import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
+import user from './auth/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth]);
+  return yield all([
+    auth,
+    user,
+  ]);
 };
